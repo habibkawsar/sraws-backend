eb0db97a2708828c6ecbb822e61c25c88c0c12ef
M. M. Habib Kawsar <habibkawsar@gmail.com>
2026-05-15 11:01:56 +0600

    Making Production Ready 3

--- obj/Release/net8.0/SponsorshipApproval.Api.AssemblyInfo.cs
+++ obj/Release/net8.0/SponsorshipApproval.Api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SponsorshipApproval.Api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2197eec1ba06c990220e6b1eb12af78f8195174c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SponsorshipApproval.Api")]
